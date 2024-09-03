06d0870a198743f983758d18126b3a6f8ee476ed
TalamsonMark <mark.talamson@strathmore.edu>
2024-09-03 09:20:10 +0300

    client portal issue

--- client/php/dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost','root','','e_drafting',3307);
-
-if(!$conn){
-    die('connection error'.mysqli_connect_error());
-}
-
-?>
--- client/php/header.php
+++ client/php/header.php
@@ -1,5 +1,5 @@
 <?php
-include 'php/dbconn.php';
+include '../php/dbconn.php';
 session_start();
 
 if(!isset($_SESSION['userid']) && !isset($_SESSION['fid'])){
