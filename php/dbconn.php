ab3bf60ff2c7087621ef3cea9c520c155bbf91b0
emtee-1023 <marktalamson@gmail.com>
2024-11-01 12:54:14 +0300

    main version

---
+++ php/dbconn.php
@@ -0,0 +1,11 @@
+<?php
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$conn = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+
+if (!$conn) {
+    die('connection error' . mysqli_connect_error());
+}
